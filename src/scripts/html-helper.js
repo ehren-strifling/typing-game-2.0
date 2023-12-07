3da653c0321e867e341645d5902066f64d2ede06
Ehren Strifling <ehrenstrifling@gmail.com>
2023-12-07 14:31:39 -0600

    create main menu draft

---
+++ src/scripts/html-helper.js
@@ -0,0 +1,14 @@
+"use strict";
+//will add this to my library if it's good enough
+
+function showClass(className) {
+	[...document.getElementsByClassName(className)].forEach(e=>{e.classList.add("show")});
+}
+
+function hideClass(className) {
+	[...document.getElementsByClassName(className)].forEach(e=>{e.classList.remove("show")});
+}
+
+function toggleClass(className) {
+	[...document.getElementsByClassName(className)].forEach(e=>{e.classList.toggle("show")});
+}
